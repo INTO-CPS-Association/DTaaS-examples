4d3523db700649bf7b8916eed9569d6ec2c689d4
prasadtalasila <prasad.talasila@gmail.com>
2023-10-23 17:02:24 +0200

    Fix three water tank DT - WIP

--- digital_twins/three-tank/lifecycle/execute.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-## Runs the main.java file
-printf "Executing the Three-Tank Digital Twin case study... \n\n"
-java -cp /workspace/examples/tools/DTManager-0.0.1-Maestro.jar /workspace/examples/tools/three-tank/TankMain.java
