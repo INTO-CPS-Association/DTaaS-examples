ef45b33af19120861e3e4968f712eccd9cfdd53b
sagilar <sagilar@unal.edu.co>
2023-12-18 15:13:11 +0100

    uploading updated flex-cell example

---
+++ digital_twins/flex-cell/readme.md
@@ -0,0 +1,103 @@
+# Flex-cell Digital Twin
+## Overview
+The flex-cell Digital Twin is a case study with two industrial robotic arms, a UR5e and a Kuka LBR iiwa 7, working in a cooperative setting on a manufacturing cell.
+
+![Flex-cell Physical Setup](flex-cell_github.jpg)
+
+The case study focuses on the robot positioning in the discrete cartesian space of the flex-cell working space.
+Therefore, it is possible to send (X,Y,Z) commands to both robots, which refer to the target hole and height they want should move to.
+
+The flex-cell case study is managed using the ```DTManager```, which is packed as a jar library in the tools, and run from a java main file.
+The ```DTManager``` uses Maestro as a slave for co-simulation, so it generates the output of the co-simulation and can interact with the real robots at the same time (with the proper configuration and setup).
+The mainfile can be changed according to the application scope, i.e., the ```/workspace/examples/tools/flex-cell/FlexCellMain.java``` can be manipulated to get a different result.
+
+The ```/workspace/examples/models/flex-cell/``` folder contains the ```.fmu``` files for the kinematic models of the robotic arms, the ```.urdf``` files for visualization (including the grippers), and the ```.aasx``` files for the schema representation with Asset Administration Shell.
+The case study also uses RabbitMQFMU to inject values into the co-simulation, therefore, there is the rabbitmqfmu in the models folder as well.
+Right now, RabbitMQFMU is only used for injecting values into the co-simulation, but not the other way around.
+The ```DTManager``` is in charge of reading the values from the co-simulation output and the current state of the physical twins.
+
+## Example Structure
+![Flex-cell system architecture with the DT Manager](flexcellRepresentation_GitHub.png)
+
+## Digital Twin Configuration
+This example uses seven models, five tools, six data, two functions, and one script. The specific assets used are:
+
+| Asset Type | Names of Assets | Visibility | Reuse in Other Examples |
+|:---|:---|:---|:---|
+| Model | kukalbriiwa_model.fmu | Private | No |
+|  | kuka_irw_gripper_rg6.urdf | Private | No |
+|  | kuka.aasx | Private | No |
+|  | ur5e_model.fmu | Private | No |
+|  | ur5e_gripper_2fg7.urdf | Private | No |
+|  | ur5e.aasx | Private | No |
+|  | rmq-vhost.fmu | Private | Yes |
+| Tool | maestro-2.3.0-jar-with-dependencies.jar | Common | Yes |
+|  | DTManager-0.0.1-Maestro.jar | Private | Yes |
+|  | urinterface | Private | No |
+|  | kukalbrinterface | Private | No |
+|  | robots_flexcell | Private | No |
+|  | FlexCellMain.java (main script) | Private | No |
+| Data | publisher-flexcell-physical.py | Private | No |
+|  | ur5e_mqtt_publisher.py | Private | No |
+|  | connections.conf | Private | No |
+|  | outputs.csv | Private | No |
+|  | kukalbriiwa7_actual.csv | Private | No |
+|  | ur5e_actual.csv | Private | No |
+| Function | plots.py | Private | No |
+|  | prepare.py | Private | No |
+
+## Lifecycle Phases
+The lifecycles that are covered include:
+1. Installation of dependencies in the create phase.
+2. Preparing the credentials for connections in the prepare phase.
+3. Execution of the experiment in the execution phase.
+4. Saving experiments in the save phase.
+5. Plotting the results of the co-simulation and the real data coming from the robots in the analyze phase.
+6. Terminating the background processes and cleaning up the outputs in the termination phase.
+
+## Run the Example
+The order the run this example is:
+1. Run the create script file with ```/workspace/examples/digital_twins/flex-cell/lifecycle/create```. In case of error, be sure the installed version of Java is OpenJDK 11, otherwise, install manually the OpenJDK 11 and use the command ```update-java-alternatives``` to set the Java version to be OpenJDK 11 and rerun the ```create``` script.
+2. Set up the credentials in the ```/workspace/examples/data/flex-cell/connections.conf``` and run the script file ```./workspace/examples/digital_twins/flex-cell/lifecycle/prepare```.
+3. Execute the Digital Twin with the script file ```/workspace/examples/digital_twins/flex-cell/lifecycle/execute```.
+4. (Optional) save the results with the script file ```/workspace/examples/digital_twins/flex-cell/lifecycle/save```.
+5. (Optional) plot the results with the script file ```/workspace/examples/digital_twins/flex-cell/lifecycle/analyze```, which saves the plots in the folder ```/workspace/examples/data/flex-cell/output```.
+6. Terminate the background processes with the script file ```/workspace/examples/digital_twins/flex-cell/lifecycle/terminate```. This one is required before running an execution again.
+7. (Optional) clean up the output folder with the script file ```/workspace/examples/digital_twins/flex-cell/lifecycle/clean```. **Remark**: This command can also remove the files in the ```physical_twin``` subfolder with the data from the real robots. See the script for further information.
+
+## Examining the results
+Executing this Digital Twin will generate a co-simulation output, but the results can also be monitored from updating the ```/workspace/examples/tools/flex-cell/FlexCellMain.java``` with a specific set of ```getAttributeValue``` commands, such as shown in the code.
+That main file enables the online execution and comparison on Digital Twin and Physical Twin at the same time and at the same abstraction level.
+
+The output is generated to the ```/workspace/examples/data/flex-cell/output``` folder.
+In case a specific experiments is to be saved, the ```save``` lifecycle script stores the co-simulation results into the ```/workspace/examples/data/flex-cell/output/saved_experiments``` folder.
+
+In the default example, the co-simulation is run for 10 seconds in steps of 0.5 seconds.
+This can be modified for a longer period and different step size.
+The output stored in ```outputs.csv``` contains the joint position of both robotic arms and the current discrete (X,Y,Z) position of the TCP of the robot.
+Additional variables can be added, such as the discrete (X,Y,Z) position of the other joints.
+
+When connected to the real robots, the tools ```urinterface``` and ```kukalbrinterface``` log their data at a higher sampling rate.
+
+
+## Connection setup
+The files ```/workspace/examples/digital_twins/flex-cell/kuka_actual.conf```, ```/workspace/examples/digital_twins/flex-cell/ur5e_actual.conf```, ```/workspace/examples/tools/flex-cell/publisher-flexcell-physical.py```, and the ```modelDescription.xml``` for the RabbitMQFMU require special credentials to connect to the RabbitMQ and the MQTT brokers.
+
+The script file  ```/workspace/examples/digital_twins/flex-cell/lifecycle/prepare``` helps you to set up the credentials and connection parameters easily.
+The only thing needed to set up the connection is to update the file ```/workspace/examples/data/flex-cell/connections.conf``` with the connection parameters for MQTT and RabbitMQ and then execute the ```prepare``` script.
+The script will set up the RabbitMQFMU and config files for this example for you.
+
+## References
+
+The [RabbitMQ FMU](https://github.com/INTO-CPS-Association/fmu-rabbitmq)
+github repository contains complete documentation and source code of
+the rmq-vhost.fmu.
+
+More information about the DT Manager and the case study is available at:
+
+```txt
+D. Lehner, S. Gil, P. H. Mikkelsen, P. G. Larsen and M. Wimmer, "An Architectural Extension for Digital Twin Platforms to Leverage Behavioral Models," 2023 IEEE 19th International Conference on Automation Science and Engineering (CASE), Auckland, New Zealand, 2023, pp. 1-8, doi: 10.1109/CASE56687.2023.10260417.
+```
+```txt
+S. Gil, P. H. Mikkelsen, D. Tola, C. Schou and P. G. Larsen, "A Modeling Approach for Composed Digital Twins in Cooperative Systems," 2023 IEEE 28th International Conference on Emerging Technologies and Factory Automation (ETFA), Sinaia, Romania, 2023, pp. 1-8, doi: 10.1109/ETFA54631.2023.10275601.
+```
---
+++ tools/flex-cell/FlexCellMain.java
@@ -0,0 +1,155 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import dtmanager.ComponentConfiguration;
+import dtmanager.DTManager;
+import dtmanager.TwinConfiguration;
+import dtmanager.TwinSchema;
+
+public class FlexCellMain {
+	static DTManager dtManager;
+	static String modelFolderPrefix = "/workspace/examples/models/flex-cell/";
+	static String dtFolderPrefix = "/workspace/examples/digital_twins/flex-cell/";
+	static String dataFolderPrefix = "/workspace/examples/data/flex-cell/";
+
+	public static void main(String[] args) {
+		TwinSchema kukaSchema = new TwinSchema(modelFolderPrefix+"kuka.aasx","Kuka_LBR_iiwa7_AAS");
+		TwinSchema ur5eSchema = new TwinSchema(modelFolderPrefix+"ur5e.aasx","UR5e_AAS");
+		
+		TwinConfiguration kukaModelConfig = new TwinConfiguration(dtFolderPrefix+"kuka_experimental.conf");
+		TwinConfiguration ur5eModelConfig = new TwinConfiguration(dtFolderPrefix+"ur5e_experimental.conf");
+		TwinConfiguration kukaActualConfig = new TwinConfiguration(dtFolderPrefix+"kuka_actual.conf");
+		TwinConfiguration ur5eActualConfig = new TwinConfiguration(dtFolderPrefix+"ur5e_actual.conf");
+		ComponentConfiguration flexcellSystemConfig = new ComponentConfiguration(dtFolderPrefix+"multimodel.json");
+		String coe = dtFolderPrefix + "coe.json";
+		String outputPath = dataFolderPrefix + "output/";
+		
+		List<TwinSchema> schemas = new ArrayList<TwinSchema>();
+		schemas.add(ur5eSchema);
+		schemas.add(kukaSchema);
+		dtManager = new DTManager("FlexcellManager",schemas);
+		dtManager.createDigitalTwin("kuka_experimental",kukaModelConfig,kukaSchema);
+		dtManager.createDigitalTwin("ur5e_experimental",ur5eModelConfig,ur5eSchema);
+		dtManager.createDigitalTwin("kuka_actual",kukaActualConfig,kukaSchema);
+		dtManager.createDigitalTwin("ur5e_actual",ur5eActualConfig,ur5eSchema);
+		
+		/***** TEST *****/
+		/*System.out.println(dtManager.getAttributeValue("Current_Joint_Position_5", "ur5e_actual"));
+		dtManager.setAttributeValue("Current_Joint_Position_5", 2.0, "ur5e_actual");
+		System.out.println(dtManager.getAttributeValue("Current_Joint_Position_5", "ur5e_actual"));*/
+		/***** END TEST *****/
+		
+		List<String> dtsFlexcellSystem = new ArrayList<String>();
+		dtsFlexcellSystem.add("kuka_experimental");
+		dtsFlexcellSystem.add("ur5e_experimental");
+		dtManager.createDigitalTwinSystem("FlexcellSystem",dtsFlexcellSystem,flexcellSystemConfig,coe,outputPath);
+		
+		List<Object> arguments = new ArrayList<Object>();
+		arguments.add(0.0);
+		dtManager.executeOperationOnSystem("initializeSimulation", arguments, "FlexcellSystem");
+		
+		dtManager.executeOperationOnSystem("simulate", null, "FlexcellSystem");
+		/*List<String> attributeNames = new ArrayList<String>();
+		List<Object> attributeValues = new ArrayList<Object>();
+		attributeNames.add("target_X_ur5e");
+		attributeNames.add("target_Y_ur5e");
+		attributeNames.add("target_Z_ur5e");
+		attributeNames.add("target_X_kuka");
+		attributeNames.add("target_Y_kuka");
+		attributeNames.add("target_Z_kuka");
+		
+		attributeValues.add(0);
+		attributeValues.add(22);
+		attributeValues.add(4);
+		attributeValues.add(4);
+		attributeValues.add(5);
+		attributeValues.add(3);
+		
+		dtManager.setSystemAttributeValues(attributeNames, attributeValues, "FlexcellSystem");*/
+		
+		/*dtManager.setSystemAttributeValue("target_X", 0, "FlexcellSystem","ur5e_experimental");
+		dtManager.setSystemAttributeValue("target_Y", 22, "FlexcellSystem","ur5e_experimental");
+		dtManager.setSystemAttributeValue("target_Z", 4, "FlexcellSystem","ur5e_experimental");
+		
+		dtManager.setSystemAttributeValue("target_X", 4, "FlexcellSystem","kuka_experimental");
+		dtManager.setSystemAttributeValue("target_Y", 5, "FlexcellSystem","kuka_experimental");
+		dtManager.setSystemAttributeValue("target_Z", 3, "FlexcellSystem","kuka_experimental");*/
+		
+		
+		Object kukaActualQ5 = dtManager.getSystemAttributeValueAt("actual_q5", "FlexcellSystem","kuka_experimental",10);
+		Object kukaActualX = dtManager.getSystemAttributeValueAt("actual_X", "FlexcellSystem","kuka_experimental",10);
+		Object kukaActualY = dtManager.getSystemAttributeValueAt("actual_Y", "FlexcellSystem","kuka_experimental",10);
+		Object kukaActualZ = dtManager.getSystemAttributeValueAt("actual_Z", "FlexcellSystem","kuka_experimental",10);
+		Object ur5eActualX = dtManager.getSystemAttributeValueAt("actual_X", "FlexcellSystem","ur5e_experimental",10);
+		Object ur5eActualY = dtManager.getSystemAttributeValueAt("actual_Y", "FlexcellSystem","ur5e_experimental",10);
+		Object ur5eActualZ = dtManager.getSystemAttributeValueAt("actual_Z", "FlexcellSystem","ur5e_experimental",10);
+		System.out.println("Kuka actual_q5 (experimental): " + kukaActualQ5.toString());
+		System.out.println("Kuka flex-cell X position (experimental): " + kukaActualX.toString());
+		System.out.println("Kuka flex-cell Y position (experimental): " + kukaActualY.toString());
+		System.out.println("Kuka flex-cell X position (experimental): " + kukaActualZ.toString());
+		System.out.println("UR5e flex-cell X position (experimental): " + ur5eActualX.toString());
+		System.out.println("UR5e flex-cell Y position (experimental): " + ur5eActualY.toString());
+		System.out.println("UR5e flex-cell Z position (experimental): " + ur5eActualZ.toString());
+		
+		/*
+		kukaActualQ5 = dtManager.getSystemAttributeValueAt("actual_q5", "FlexcellSystem","kuka_experimental",10);
+		kukaActualX = dtManager.getSystemAttributeValueAt("actual_X", "FlexcellSystem","kuka_experimental",10);
+		kukaActualY = dtManager.getSystemAttributeValueAt("actual_Y", "FlexcellSystem","kuka_experimental",10);
+		kukaActualZ = dtManager.getSystemAttributeValueAt("actual_Z", "FlexcellSystem","kuka_experimental",10);
+		ur5eActualX = dtManager.getSystemAttributeValueAt("actual_X", "FlexcellSystem","ur5e_experimental",10);
+		ur5eActualY = dtManager.getSystemAttributeValueAt("actual_Y", "FlexcellSystem","ur5e_experimental",10);
+		ur5eActualZ = dtManager.getSystemAttributeValueAt("actual_Z", "FlexcellSystem","ur5e_experimental",10);
+		System.out.println(kukaActualQ5.toString());
+		System.out.println(kukaActualX.toString());
+		System.out.println(kukaActualY.toString());
+		System.out.println(kukaActualZ.toString());
+		System.out.println(ur5eActualX.toString());
+		System.out.println(ur5eActualY.toString());
+		System.out.println(ur5eActualZ.toString());*/
+		
+		
+		Object value = dtManager.getAttributeValue("actual_q_5", "ur5e_actual");
+		System.out.println("UR5e actual_q_5 (actual): " + value);
+		Object ur5eExperimentalQ = dtManager.getSystemAttributeValueAt("actual_q5", "FlexcellSystem","ur5e_experimental",10);
+		System.out.println("UR5e actual_q_5 (experimental): " + ur5eExperimentalQ);
+		value = dtManager.getAttributeValue("actual_q_0", "ur5e_actual");
+		ur5eExperimentalQ = dtManager.getSystemAttributeValueAt("actual_q0", "FlexcellSystem","ur5e_experimental",10);
+		System.out.println("UR5e actual_q_0 (actual): " + value);
+		System.out.println("UR5e actual_q_0 (experimental): " + ur5eExperimentalQ);
+
+		Object valueK = dtManager.getAttributeValue("actual_q_5", "kuka_actual");
+		System.out.println("kuka actual_q_5 (actual): " + valueK);
+		Object kukaExperimentalQ = dtManager.getSystemAttributeValueAt("actual_q5", "FlexcellSystem","kuka_experimental",10);
+		System.out.println("kuka actual_q_5 (experimental): " + kukaExperimentalQ);
+		valueK = dtManager.getAttributeValue("actual_q_0", "kuka_actual");
+		kukaExperimentalQ = dtManager.getSystemAttributeValueAt("actual_q0", "FlexcellSystem","kuka_experimental",10);
+		System.out.println("kuka actual_q_0 (actual): " + valueK);
+		System.out.println("kuka actual_q_0 (experimental): " + kukaExperimentalQ);
+		
+		/*
+
+		Thread eventThread = new Thread(() -> {
+			new Timer().scheduleAtFixedRate(new TimerTask() {
+				public void run() {
+					try {
+						Object value = dtManager.getAttributeValue("actual_q_5", "ur5e_actual");
+						System.out.println("UR5e actual_q_5 (actual): " + value);
+						Object ur5eExperimentalQ = dtManager.getSystemAttributeValueAt("actual_q5", "FlexcellSystem","ur5e_experimental",10);
+						System.out.println("UR5e actual_q_5 (experimental): " + ur5eExperimentalQ);
+						value = dtManager.getAttributeValue("actual_q_0", "ur5e_actual");
+						ur5eExperimentalQ = dtManager.getSystemAttributeValueAt("actual_q0", "FlexcellSystem","ur5e_experimental",10);
+						System.out.println("UR5e actual_q_0 (actual): " + value);
+						System.out.println("UR5e actual_q_0 (experimental): " + ur5eExperimentalQ);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}, 1000, 1000);
+		});
+		eventThread.setDaemon(true);
+		eventThread.start();*/
+	}
+
+}
