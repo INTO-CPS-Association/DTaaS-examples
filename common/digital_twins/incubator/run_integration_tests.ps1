45dca5adee452302f198d8d1fbcea8d30a7d71ae
Morten Haahr Kristensen <mhk@ece.au.dk>
2024-03-14 14:26:37 +0100

    Removed integration_tests

--- common/digital_twins/incubator/run_integration_tests.ps1
+++
@@ -1,5 +0,0 @@
-# Set environment variable CLIMODE, so that tests know that they should not plot stuff.
-$Env:CLIMODE = "ON"
-
-# Run unittest in discovery mode for the integration_tests folder
-& python -m unittest discover --failfast -v integration_tests -p "*.py"
