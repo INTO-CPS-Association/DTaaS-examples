db52583df1fa22c10b2ce9f288e3151ae3c13d73
Alberto Bonizzi <aseoista@gmail.com>
2024-05-14 15:44:20 +0200

    Move m1.smv and safe-operation.msv

--- digital_twins/water_tank_FI_monitor/lifecycle/synthesis_m1_fmu.cmd
+++ digital_twins/water_tank_FI_monitor/lifecycle/synthesis_m1_fmu.cmd
@@ -1,5 +1,5 @@
-set input_file "/workspace/examples/models/m1.smv"
+set input_file "/workspace/examples/digital_twins/water_tank_FI_monitor/model/m1.smv"
 go
-build_monitor -n 0 -C "/workspace/examples/models/observables_m1.list"
+build_monitor -n 0 -C "/workspace/examples/digital_twins/water_tank_FI_monitor/model/observables_m1.list"
 generate_monitor -n 0 -l 3 -L "FMU" -o "m1"
 quit
