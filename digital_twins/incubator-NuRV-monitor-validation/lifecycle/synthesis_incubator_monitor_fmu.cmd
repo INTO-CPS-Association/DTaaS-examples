a39f12ff8c2c145b89d66542ad053861488463d2
Alberto Bonizzi <aseoista@gmail.com>
2024-05-15 14:28:07 +0200

    Move SMV model

--- digital_twins/incubator-NuRV-monitor-validation/lifecycle/synthesis_incubator_monitor_fmu.cmd
+++ digital_twins/incubator-NuRV-monitor-validation/lifecycle/synthesis_incubator_monitor_fmu.cmd
@@ -1,4 +1,4 @@
-set input_file "/workspace/examples/digital_twins/incubator-NuRV-monitor-validation/safe-operation.smv"
+set input_file "/workspace/examples/digital_twins/incubator-NuRV-monitor-validation/model/safe-operation.smv"
 go
 build_monitor -n 0
 generate_monitor -n 0 -l 3 -L "FMU" -o "safe-operation"
