a03bf4c1a5c8efd8af661570046aa853c82de194
Alberto Bonizzi <aseoista@gmail.com>
2024-05-21 16:01:01 +0200

    Minor improvements in water_tank_FI_monitor plot

--- digital_twins/water_tank_FI_monitor/lifecycle/plot.py
+++ digital_twins/water_tank_FI_monitor/lifecycle/plot.py
@@ -12,14 +12,16 @@ name1='{x2}.tank.level'
 name4='{x2}.tank.valvecontrol'
 monitor='{x4}.m1._output'
 ax.title.set_text('tank level - FI with monitor')
-ax.plot(df0['time'], df0[name1])
-ax.plot(df0['time'], df0[name4])
-ax.plot(df0['time'], df0[monitor])
-ax.plot(df0['time'], [2.2] * len(df0['time']), '--')
+ax.plot(df0['time'], df0[name1], color='tab:blue', label='water level')
+ax.plot(df0['time'], df0[name4], color='tab:orange', label='control output')
+ax.plot(df0['time'], df0[monitor], color='tab:green', label='monitor output')
+ax.plot(df0['time'], [2.2] * len(df0['time']), '--', color='tab:red',
+        label='monitor threshold')
 plt.yticks(np.arange(0.0, 9.2, 1.0))
 ax.set_xlim([0, 40])
 ax.grid()
 fig.tight_layout()
 plt.rcParams['figure.figsize'] = [12, 10]
+plt.legend()
 #plt.show()
 plt.savefig('resultFI_mon.png')
